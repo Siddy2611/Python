78922c305986e52ce864e7cea1a0e2df786961e6
Siddy2611 <siddy2611@gmail.com>
2025-08-07 12:04:29 +0530

    Updated

--- Day2/functions.py
+++ Day2/functions.py
@@ -1,17 +1,32 @@
-def hello():
-    print("Good Morning")
+# def hello():
+#     print("Good Morning")
 
-hello()
+# hello()
 
 
-def greet(name="Siddharth"):
-    print(f"I am {name}")
+# def greet(name="Siddharth"):
+#     print(f"I am {name}")
 
-greet()#default arguments
-greet("Sid") #positional arguments
+# greet()#default arguments
+# greet("Sid") #positional arguments
 
-def details(name,age):
-    print(f"I am {name} and my age is {age}")
+# def details(name,age):
+#     print(f"I am {name} and my age is {age}")
 
-details(12,"Siddharth")
-details(age=21,name="Sid") #Keyword arguments
+# details(12,"Siddharth")
+# details(age=21,name="Sid") #Keyword arguments
+
+
+
+def reverse(str):
+    rev=""
+    for i in range(len(str)-1,-1,-1):
+        rev+=str[i]
+
+    if(rev==str):
+        print("It is Palindrome")
+    else:
+        print("It is not Palindrome")
+
+word=input("Enter word: ")
+reverse(word)
